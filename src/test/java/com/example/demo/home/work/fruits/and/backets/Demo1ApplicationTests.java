37439912fb4318ee9cede53867ec5ebaba38d27b
Stupendos <topakovdenis671@gmail.com>
2024-10-24 19:17:11 +0700

    ASD

---
+++ src/test/java/com/example/demo/home/work/fruits/and/backets/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.home.work.fruits.and.backets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
